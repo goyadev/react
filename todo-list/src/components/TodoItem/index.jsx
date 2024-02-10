73b530c7ea6ab920aa1fb9886e87ca8899243400
goyadev <goyazheng@gmail.com>
2024-02-10 12:31:15 +0800

    updated adding new item to list and added complete button and added delete button

--- todo-list/src/components/TodoItem/index.jsx
+++ todo-list/src/components/TodoItem/index.jsx
@@ -1,4 +1,16 @@
+import "./style.css";
+
 function TodoItem(props) {
-  return <div>{props.text}</div>;
+  return (
+    <div className={`todo ${props.todo.isCompleted ? "complete" : ""}`}>
+      {props.todo.text}
+      <div>
+        <button onClick={() => props.completeTodo(props.index)}>
+          {props.todo.isCompleted ? "Completed" : "Incomplete"}
+        </button>
+        <button onClick={() => props.removeTodo(props.index)}>x</button>
+      </div>
+    </div>
+  );
 }
 export default TodoItem;
